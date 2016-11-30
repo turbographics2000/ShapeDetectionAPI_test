26606526ccd900c68dfb296320898564c50c4a99
turbographics2000 <turbographics2000@yahoo.co.jp>
2016-12-01 07:34:58 +0900

    commit

---
+++ docs/test.js
@@ -0,0 +1,48 @@
+let barcodeNames = [
+    '39',
+    'codabar',
+    'code128',
+    'Databar_1-300x122',
+    'EAN',
+    'gtin14',
+    'i25',
+    'pdf4172',
+    'postnet', 'qrcode',
+    'ucc128',
+    'upcA',
+    'upcE'
+];
+let barcodeDetector = new BarcodeDetector();
+
+Promise.all(barcodeNames.map(name => {
+    return new Promise((resolve, reject) => {
+        let img = new Image();
+        img.onload = function () {
+            resolve(img);
+        }
+        img.src = name + '.png';
+    });
+})).then(barcodeImages => {
+    Promise.all(barcodeImages.map(img => {
+        barcodeDetector.detect(img);
+    })).then(detectBarcords => {
+        detectBarcords.forEach((result, i) => {
+            let li = (text, parent) => {
+                let elm = document.createElement('li');
+                elm.textContent = text;
+                parent.appendChild(elm);
+            }
+            let resultItem = document.createElement('li');
+            let rawValue = 'rawValue: ' + document.createElement('div');
+            let boundingBox = document.createElement('ul');
+            boundingBox.classList.add('boundingbox');
+            li('x: ' + result.boundingBox.x, boundingBox);
+            li('y: ' + result.boundingBox.y, boundingBox);
+            li('w: ' + result.boundingBox.w, boundingBox);
+            li('h: ' + result.boundingBox.h, boundingBox);
+            resultItem.appendChild(rawValue);
+            resultItem.appendChild(barcodeImages[i]);
+            resultItem.appendChild(boundingBox);
+        });
+    });
+});
